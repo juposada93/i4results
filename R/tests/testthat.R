243bcf5d2fa0f93a174a1dd284262af9c902f042
Juan Pablo <juposada93@gmail.com>
2025-07-22 18:43:09 -0500

    Remove remotes::install_github from test setup
    Eliminates the installation of the i4results package via remotes::install_github in testthat.R. Assumes the package is already installed, simplifying the test setup process.

--- R/tests/testthat.R
+++ R/tests/testthat.R
@@ -1,10 +1,4 @@
 library(testthat)
-remotes::install_github(
-  "juposada93/i4results",
-  subdir       = "R",                  # porque DESCRIPTION está allí
-  INSTALL_opts = "--install-tests"     # para que test_check funcione
-)
-
 library(i4results)
 test_package("i4results")
 
